d6445f2709ac994d027c53ef0f388df7bdcbb788
kendrickPC <kennethpchang@gmail.com>
2021-08-17 11:59:02 -0700

    Flashcards loaded and flipped on screen

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
+import React, { useState } from 'react';
+import FlashcardList from './FlashcardList';
+
 function App() {
+  const [ flashcards, setFlashcards ] = useState(SAMPLE_FLASHCARDS);
   return (
-    <h1>Hello World!</h1>
+    // <h1>Hello World!</h1>
+    <FlashcardList flashcards={flashcards} />
   );
 }
+
+const SAMPLE_FLASHCARDS = [
+  {
+    id: 1,
+    question: "Who do you think you are?",
+    answer: "Kenderson",
+    options: [
+      "Jesus",
+      "Kenderson",
+      "Messiah",
+      "BTS",
+    ]
+  },
+  {
+    id: 2,
+    question: "Who do you think God is?",
+    answer: "Kenderson",
+    options: [
+      "Jesus",
+      "Kenderson",
+      "Messiah",
+      "BTS",
+    ]
+  },
+]
 
 export default App;
